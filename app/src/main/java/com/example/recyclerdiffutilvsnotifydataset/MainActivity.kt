05b27432377289d01015df0189eb867c08e05f2d
leonardosibela <leonardo_sibela@hotmail.com>
2021-02-12 12:25:21 -0300

    Displaying characters with notifyDatasetChanged

--- app/src/main/java/com/example/recyclerdiffutilvsnotifydataset/MainActivity.kt
+++ app/src/main/java/com/example/recyclerdiffutilvsnotifydataset/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.recyclerdiffutilvsnotifydataset
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private var characterAdapter = CharacterNotifyAdapter(listOf())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setupRecyclerView()
+        fetchDataWithNotifyDataseChanged()
+    }
+
+    private fun setupRecyclerView() {
+        characters_recycler.apply {
+            setHasFixedSize(true)
+            adapter = characterAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
+        }
+    }
+
+    private fun fetchDataWithNotifyDataseChanged() {
+        getCharacters()
+        characterAdapter.characters = getCharacters()
+    }
+
+    private fun getCharacters(): List<Character> {
+        return listOf(
+            Character(1, "Spider Man"),
+            Character(2, "Iron Man"),
+            Character(3, "Hulk"),
+            Character(4, "Captain America"),
+            Character(5, "Thanos"),
+            Character(6, "Wolverine"),
+            Character(7, "Deadpool"),
+            Character(8, "Dr. Strange"),
+            Character(9, "Black Widow"),
+            Character(10, "Thor"),
+            Character(11, "Vision"),
+            Character(12, "Nick Fury"),
+            Character(13, "Venom"),
+            Character(14, "Galactus"),
+            Character(15, "Punisher")
+        )
     }
 }
