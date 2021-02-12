ae5add25cb1b518a93ee9cd3f94aeac0cc2e0d94
leonardosibela <leonardo_sibela@hotmail.com>
2021-02-12 12:50:08 -0300

    Fetching two different types of data

--- app/src/main/java/com/example/recyclerdiffutilvsnotifydataset/MainActivity.kt
+++ app/src/main/java/com/example/recyclerdiffutilvsnotifydataset/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.example.recyclerdiffutilvsnotifydataset
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
+const val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private var characterAdapter = CharacterNotifyAdapter(listOf())
+    private var fetchesAmount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupRecyclerView()
         fetchDataWithNotifyDatasetChanged()
+        setupViews()
     }
 
     private fun setupRecyclerView() {
@@ -24,11 +29,36 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     }
 
     private fun fetchDataWithNotifyDatasetChanged() {
-        getCharacters()
-        characterAdapter.setCharactersWithNotifyDataSetChanged(getCharacters())
+        displayNewCharacters()
     }
 
-    private fun getCharacters(): List<Character> {
+    private fun displayNewCharacters() {
+        val characters = getCharacters()
+        characterAdapter.setCharactersWithNotifyDataSetChanged(characters)
+    }
+
+    private fun setupViews() {
+        Log.d(TAG, "setupViews()")
+        setupChangeButton()
+    }
+
+    private fun setupChangeButton() {
+        Log.d(TAG, "setupChangeButton()")
+        change_button.setOnClickListener {
+            Log.d(TAG, "change_button.OnClickListener")
+            displayNewCharacters()
+        }
+    }
+
+    private fun getCharacters() = if (fetchesAmount++ % 2 == 0) {
+        Log.d(TAG, "setupChangeButton()")
+        getCharactersOdd()
+    } else {
+        getCharactersEven()
+    }
+
+    private fun getCharactersOdd(): List<Character> {
+        Log.d(TAG, "getCharactersOdd()")
         return listOf(
             Character(1, "Spider Man"),
             Character(2, "Iron Man"),
@@ -45,6 +75,22 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             Character(13, "Venom"),
             Character(14, "Galactus"),
             Character(15, "Punisher")
+        )
+    }
+
+    private fun getCharactersEven(): List<Character> {
+        Log.d(TAG, "getCharactersEven()")
+        return listOf(
+            Character(1, "Spider Man"),
+            Character(2, "Iron Man"),
+            Character(3, "Hulk"),
+            Character(4, "Captain America"),
+            Character(5, "Thanos"),
+            Character(11, "VISION"),
+            Character(12, "NICK FURY"),
+            Character(13, "VENOM"),
+            Character(14, "GALACTUS"),
+            Character(15, "PUNISHER")
         )
     }
 }
